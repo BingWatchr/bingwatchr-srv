43aee511d5791e67eb11aa6700920a9bd44483e7
Fabio Sdringola <fabio.sdringola@gmail.com>
2023-06-30 15:01:17 +0200

    fix

--- bin/seed.js
+++ bin/seed.js
@@ -1,34 +1,43 @@
-const mongoose = require('mongoose');
-const axios = require('axios');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const axios = require("axios");
+const dotenv = require("dotenv");
 const MONGO_URI =
-	process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bingewatchr-srv';
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/bingewatchr-srv";
 
 mongoose.connect(MONGO_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', async () => {
-	console.log('Connected to MongoDB');
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", async () => {
+  console.log("Connected to MongoDB");
 
-	try {
-		const response = await axios.get('https://api.tvmaze.com/shows');
-		const shows = response.data;
+  try {
+    const response = await axios.get("https://api.tvmaze.com/shows");
+    const shows = response.data;
 
-		// Clear existing data from the collection
-		await db.collection('shows').deleteMany({});
+	const { image: { medium, ...rest }, ...restData } = shows;
 
-		// Save the fetched shows to the database
-		await db.collection('shows').insertMany(shows);
+	const updatedData = {
+	  ...restData,
+	  image: medium,
+	  ...rest
+	};
+  
+console.log(updatedData)
+    // Clear existing data from the collection
+    await db.collection("shows").deleteMany({});
 
-		console.log('Shows seeded successfully');
-		db.close();
-	} catch (error) {
-		console.error('Error seeding shows:', error);
-		db.close();
-	}
+    // Save the fetched shows to the database
+    await db.collection("shows").insertMany(updatedData);
+
+    console.log("Shows seeded successfully");
+    db.close();
+  } catch (error) {
+    console.error("Error seeding shows:", error);
+    db.close();
+  }
 });
--- routes/review.routes.js
+++ routes/review.routes.js
@@ -1,82 +1,82 @@
-const router = require('express').Router();
-const mongoose = require('mongoose');
+const router = require("express").Router();
+const mongoose = require("mongoose");
 
-const Show = require('../models/Show.model');
-const Review = require('../models/Review.model');
+const Show = require("../models/Show.model");
+const Review = require("../models/Review.model");
 
 //POST create a new review
 //we can change this  if we want to create a new review directly from the show
-router.post('/reviews', (req, res, next) => {
-	const { author, text, rating, showId } = req.body;
+router.post("/reviews", (req, res, next) => {
+  const { author, text, rating, showId } = req.body;
 
-	const newReview = {
-		author: author,
-		text: text,
-		rating: rating,
-		tvShow: showId,
-	};
+  const newReview = {
+    author: author,
+    text: text,
+    rating: rating,
+    tvShow: showId,
+  };
 
-	Review.create(newReview)
-		.then((reviewsFromDB) => {
-			return Show.findByIdAndUpdate(showId, {
-				$push: { reviews: reviewsFromDB._id },
-			});
-		})
-		.then((response) => res.status(201).json(response))
-		.catch((e) => {
-			console.log(e);
-			res.status(500).json({ message: 'error creating review', error: e });
-		});
+  Review.create(newReview)
+    .then((reviewsFromDB) => {
+      return Show.findByIdAndUpdate(showId, {
+        $push: { reviews: reviewsFromDB._id },
+      });
+    })
+    .then((response) => res.status(201).json(response))
+    .catch((e) => {
+      console.log(e);
+      res.status(500).json({ message: "error creating review", error: e });
+    });
 });
 
 // PUT  Updates a specific review by id
-router.put('/reviews/:reviewId', (req, res, next) => {
-	const { reviewId } = req.params;
+router.put("/reviews/:reviewId", (req, res, next) => {
+  const { reviewId } = req.params;
 
-	if (!mongoose.Types.ObjectId.isValid(reviewId)) {
-		res.status(400).json({ message: 'Specified id is not valid' });
-		return;
-	}
+  if (!mongoose.Types.ObjectId.isValid(reviewId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
 
-	const newDetails = {
-		text: text,
-		rating: rating,
-	};
+  const newDetails = {
+    text: text,
+    rating: rating,
+  };
 
-	Project.findByIdAndUpdate(reviewId, newDetails, { new: true })
-		.then((updatedReview) => res.json(updatedReview))
-		.catch((e) => {
-			console.log('error updating review', e);
-			res.status(500).json({
-				message: 'error updating review',
-				error: e,
-			});
-		});
+  Project.findByIdAndUpdate(reviewId, newDetails, { new: true })
+    .then((updatedReview) => res.json(updatedReview))
+    .catch((e) => {
+      console.log("error updating review", e);
+      res.status(500).json({
+        message: "error updating review",
+        error: e,
+      });
+    });
 });
 
 // DELETE  Delete a specific review by id
-router.delete('/reviews/:reviewId', (req, res, next) => {
-	const { reviewId } = req.params;
+router.delete("/reviews/:reviewId", (req, res, next) => {
+  const { reviewId } = req.params;
 
-	if (!mongoose.Types.ObjectId.isValid(reviewId)) {
-		res.status(400).json({ message: 'Specified id is not valid' });
-		return;
-	}
+  if (!mongoose.Types.ObjectId.isValid(reviewId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
 
-	Project.findByIdAndRemove(reviewId)
-		.then((deletedReview) => {})
-		.then(() =>
-			res.json({
-				message: `Review with id ${reviewId} was removed successfully.`,
-			})
-		)
-		.catch((e) => {
-			console.log('error deleting review', e);
-			res.status(500).json({
-				message: 'error deleting review',
-				error: e,
-			});
-		});
+  Project.findByIdAndRemove(reviewId)
+    .then((deletedReview) => {})
+    .then(() =>
+      res.json({
+        message: `Review with id ${reviewId} was removed successfully.`,
+      })
+    )
+    .catch((e) => {
+      console.log("error deleting review", e);
+      res.status(500).json({
+        message: "error deleting review",
+        error: e,
+      });
+    });
 });
 
 module.exports = router;
